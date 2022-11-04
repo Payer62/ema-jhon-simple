a23c63934887c26daae6a0fc4a136dc701e1ff76
Payer Hossain <you@example.com>
2022-11-04 20:41:25 +0600

    full_project_done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import './Cart.css';
 
-const Cart = ({cart}) => {
+const Cart = (props) => {
+  const {cart} = props;
+
+
+  let total = 0;
+  let shipping = 0;
+  let quantity = 0;
+  for(const product of cart){
+    total = total + product.price * product.quantity;
+    quantity = quantity + product.quantity;
+    shipping = shipping + product.shipping;
+  };
+  const tax = parseFloat((total * 0.1).toFixed(2));
+  const grandTotal = total + shipping + tax;
+
     return (
-      <div className="cart-info">
+      <div className="cart">
         <h3 className="cart-title">Order Summary</h3>
-        <div>
-          <p>Selected Item: {cart.length}</p>
-        </div>
+          <p>Selected Item: {quantity}</p>
+          <p>Total Price: {total}</p>
+          <p>Total Shipping: {shipping}</p>
+          <p>Tax: {tax}</p>
+          <h5>Grand Total: {grandTotal.toFixed(2)}</h5> 
       </div>
     );
 };
